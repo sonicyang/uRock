b9cb710e11a13a07e6b8974ce2f2dd732a1abe4a
sonicyang <sonic.tw.tp@gmail.com>
2014-12-10 16:55:38 +0800

    Refactor ftoa and ito a into  a file

---
+++ inc/helper.h
@@ -0,0 +1,10 @@
+#ifndef __HELPRE_H__
+#define __HELPER_H__
+
+void reverse(char s[]);
+void itoa(int n, char s[]);
+
+int intToStr(int x, char str[], int d);
+void ftoa(float n, char *res, int afterpoint);
+
+#endif //__HELPER_H__
---
+++ src/helper.c
@@ -0,0 +1,75 @@
+#include "string.h"
+#include "math.h"
+#include "helper.h"
+
+void reverse(char s[])
+{
+    int i, j;
+    char c;
+
+    for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+void itoa(int n, char s[])
+{
+    int i, sign;
+
+    if ((sign = n) < 0)  /* record sign */
+        n = -n;          /* make n positive */
+    i = 0;
+    do {       /* generate digits in reverse order */
+        s[i++] = n % 10 + '0';   /* get next digit */
+    } while ((n /= 10) > 0);     /* delete it */
+    if (sign < 0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
+
+int intToStr(int x, char str[], int d)
+{
+    int i = 0;
+    while (x)
+    {
+        str[i++] = (x%10) + '0';
+        x = x/10;
+    }
+ 
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < d)
+        str[i++] = '0';
+ 
+    str[i] = '\0';
+    reverse(str);
+    return i;
+}
+
+void ftoa(float n, char *res, int afterpoint)
+{
+    // Extract integer part
+    int ipart = (int)n;
+ 
+    // Extract floating part
+    float fpart = n - (float)ipart;
+ 
+    // convert integer part to string
+    int i = intToStr(ipart, res, 0);
+ 
+    // check for display option after point
+    if (afterpoint != 0)
+    {
+        res[i] = '.';  // add dot
+ 
+        // Get the value of fraction part upto given no.
+        // of points after dot. The third parameter is needed
+        // to handle cases like 233.007
+        fpart = fpart * pow(10, afterpoint);
+ 
+        intToStr((int)fpart, res + i + 1, afterpoint);
+    }
+}
