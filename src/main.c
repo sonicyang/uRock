6786095de9117bcb45531ac39e67fbbc861af4db
TheKK <thumbd03803@gmail.com>
2014-12-13 21:41:37 +0800

    Clean code

--- src/main.c
+++ src/main.c
@@ -16,8 +16,8 @@
  *
  *        http://www.st.com/software_license_agreement_liberty_v2
  *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -68,42 +68,48 @@ static void LED_Thread1(void const *argument);
 osThreadId SPUid;
 static void SignalProcessingUnit(void const *argument);
 volatile uint32_t SPU_Hold = 0;
-volatile uint16_t SignalBuffer[BUFFER_NUM][SAMPLE_NUM]; 
+volatile uint16_t SignalBuffer[BUFFER_NUM][SAMPLE_NUM];
 volatile float SignalPipe[STAGE_NUM][SAMPLE_NUM];
 struct Effect_t *EffectStages[STAGE_NUM];
 
 osThreadId UIid;
 static void UserInterface(void const *argument);
 
+osThreadId InputEventId;
+static void InputEvent(void const *argument);
+
+uint32_t controllingStage = 0;
+uint8_t values[3];
+
 int main(void){
-	/* STM32F4xx HAL library initialization:
-	   - Configure the Flash prefetch, instruction and Data caches
-	   - Configure the Systick to generate an interrupt each 1 msec
-	   - Set NVIC Group Priority to 4
-	   - Global MSP (MCU Support Package) initialization
-	   */
+    /* STM32F4xx HAL library initialization:
+       - Configure the Flash prefetch, instruction and Data caches
+       - Configure the Systick to generate an interrupt each 1 msec
+       - Set NVIC Group Priority to 4
+       - Global MSP (MCU Support Package) initialization
+       */
 
-	/* Configure the system clock to 180 Mhz */
-	SystemClock_Config();
+    /* Configure the system clock to 180 Mhz */
+    SystemClock_Config();
 
-	HAL_Init();
+    HAL_Init();
 
-	BSP_LCD_Init();
-	BSP_LCD_LayerDefaultInit(LCD_FOREGROUND_LAYER, LCD_FRAME_BUFFER);
-	BSP_LCD_LayerDefaultInit(LCD_BACKGROUND_LAYER, LCD_FRAME_BUFFER + BUFFER_OFFSET);
-	BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
-	BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
+    BSP_LCD_Init();
+    BSP_LCD_LayerDefaultInit(LCD_FOREGROUND_LAYER, LCD_FRAME_BUFFER);
+    BSP_LCD_LayerDefaultInit(LCD_BACKGROUND_LAYER, LCD_FRAME_BUFFER + BUFFER_OFFSET);
+    BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
+    BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
 
-	BSP_LCD_SelectLayer(LCD_FOREGROUND_LAYER);
-	BSP_LCD_Clear(LCD_COLOR_RED);
-	BSP_LCD_FillCircle(150, 150, 40);
+    BSP_LCD_SelectLayer(LCD_FOREGROUND_LAYER);
+    BSP_LCD_Clear(LCD_COLOR_RED);
+    BSP_LCD_FillCircle(150, 150, 40);
 
-	BSP_TS_Init(BSP_LCD_GetXSize(), BSP_LCD_GetYSize());
+    BSP_TS_Init(BSP_LCD_GetXSize(), BSP_LCD_GetYSize());
 
-	BSP_SDRAM_Init();
+    BSP_SDRAM_Init();
 
-	BSP_LED_Init(LED3);
-	BSP_LED_Init(LED4);
+    BSP_LED_Init(LED3);
+    BSP_LED_Init(LED4);
 
     MX_GPIO_Init();
     MX_DMA_Init();
@@ -112,30 +118,33 @@ int main(void){
     MX_ADC2_Init();
     MX_DAC_Init();
 
-	osThreadDef(LED3, LED_Thread1, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
-	LEDThread1Handle = osThreadCreate (osThread(LED3), NULL);
+    osThreadDef(LED3, LED_Thread1, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
+    LEDThread1Handle = osThreadCreate (osThread(LED3), NULL);
 
-	osThreadDef(SPU, SignalProcessingUnit, osPriorityNormal, 0, 1024);
+    osThreadDef(SPU, SignalProcessingUnit, osPriorityNormal, 0, 1024);
     SPUid = osThreadCreate (osThread(SPU), NULL);
 
-	osThreadDef(UI, UserInterface, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
+    osThreadDef(UI, UserInterface, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
     UIid = osThreadCreate (osThread(UI), NULL);
 
-	osKernelStart (NULL, NULL);
+    osThreadDef(IE, InputEvent, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
+    UIid = osThreadCreate (osThread(IE), NULL);
 
-	while (1);
+    osKernelStart (NULL, NULL);
+
+    while (1);
 }
 
 static void SignalProcessingUnit(void const *argument){
     uint32_t index = 0;
     uint32_t pipeindex = 0;
     uint32_t i;
-    
+
     for(i = 0; i < STAGE_NUM; i++){
         EffectStages[i] = NULL;
     }
 
-    /* Effect Stage Setting*/ 
+    /* Effect Stage Setting*/
 
     //EffectStages[0] = new_Volume(&vol);
     //EffectStages[0] = new_Distortion(&distor);
@@ -145,14 +154,12 @@ static void SignalProcessingUnit(void const *argument){
     HAL_TIM_Base_Start(&htim2);
     HAL_ADC_Start_DMA_DoubleBuffer(&hadc1, (uint32_t*)SignalBuffer[0], (uint32_t*)SignalBuffer[1], SAMPLE_NUM);
     HAL_DAC_Start_DMA_DoubleBuffer(&hdac, DAC_CHANNEL_2, (uint32_t*) SignalBuffer[1], (uint32_t*) SignalBuffer[2], SAMPLE_NUM, DAC_ALIGN_12B_R);
-  
+
     /* Process */
     while(1){
         if(SPU_Hold){
             SPU_Hold--;
 
-            NormalizeData(SignalBuffer[index], SignalPipe[pipeindex]);
-            
             for(i = 0; i < STAGE_NUM; i++){
                 if(EffectStages[i] != NULL){
                     EffectStages[i]->func(SignalPipe[(pipeindex - i) % STAGE_NUM], EffectStages[i]);
@@ -174,25 +181,7 @@ static void SignalProcessingUnit(void const *argument){
 }
 
 static void UserInterface(void const *argument){
-    uint8_t values[3];
-    char buf[16];
-
-	TS_StateTypeDef tp;
-    uint32_t controllingStage = 0;
-
-    HAL_ADC_Start_DMA(&hadc2, (uint32_t*)values, 3);
-    BSP_LCD_Clear(LCD_COLOR_WHITE);
-    BSP_LCD_DisplayStringAt(0, 0, (uint8_t*) "uROCK", CENTER_MODE);
-    
-    osDelay(10);
-	while (1) {
-		BSP_TS_GetState(&tp);
-		if (tp.TouchDetected == 1) {
-            controllingStage++;
-            if(controllingStage >= 4)
-                controllingStage = 0;
-		}
-
+    while (1) {
         BSP_LCD_Clear(LCD_COLOR_WHITE);
 
         if(EffectStages[controllingStage] == NULL){
@@ -204,26 +193,37 @@ static void UserInterface(void const *argument){
             EffectStages[controllingStage]->adj(EffectStages[controllingStage], values);
         }
 
-       /* 
-        ftoa(EffectStages[controllingStage].parameter[0].value, buf, 2);
-        BSP_LCD_DisplayStringAt(0, 3 * 16, (uint8_t*) buf, CENTER_MODE);
-        ftoa(EffectStages[controllingStage].parameter[1].value, buf, 2);
-        BSP_LCD_DisplayStringAt(0, 4 * 16, (uint8_t*) buf, CENTER_MODE);
-        ftoa(EffectStages[controllingStage].parameter[2].value, buf, 2);
-        BSP_LCD_DisplayStringAt(0, 5 * 16, (uint8_t*) buf, CENTER_MODE);
-        */
+        osSignalWait(0, 0);
+    }
+}
 
-        osDelay(200);
+static void InputEvent(void const *argument){
+    TS_StateTypeDef tp;
+
+    HAL_ADC_Start_DMA(&hadc2, (uint32_t*)values, 3);
+
+    while (1) {
+        BSP_TS_GetState(&tp);
+        if (tp.TouchDetected == 1) {
+            controllingStage++;
+
+            if(controllingStage >= 4)
+                controllingStage = 0;
+
+           osSignalSet(UIid, 0);
+        }
+
+        osDelay(50);
     }
 }
 
 static void LED_Thread1(void const *argument){
-	(void) argument;
+    (void) argument;
 
-	for(;;){
-		osDelay(300);
-		BSP_LED_Toggle(LED3);
-	}
+    for(;;){
+        osDelay(300);
+        BSP_LED_Toggle(LED3);
+    }
 }
 
 /* Double Buffer Swapping Callbacks */
@@ -248,7 +248,7 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
 
         SPU_Hold++;
     }
-    return;  
+    return;
 }
 
 void HAL_ADC_ConvM1CpltCallback(ADC_HandleTypeDef* hadc){
@@ -260,10 +260,10 @@ void HAL_ADC_ConvM1CpltCallback(ADC_HandleTypeDef* hadc){
             index = 1;
 
         HAL_DMAEx_ChangeMemory(&hdma_adc1, (uint32_t)SignalBuffer[index], MEMORY1);
-        
+
         SPU_Hold++;
     }
-    return;  
+    return;
 }
 
 void HAL_DACEx_ConvCpltCallbackCh2(DAC_HandleTypeDef* hdac){
@@ -275,7 +275,7 @@ void HAL_DACEx_ConvCpltCallbackCh2(DAC_HandleTypeDef* hdac){
 
     HAL_DMAEx_ChangeMemory(&hdma_dac2, (uint32_t)SignalBuffer[index], MEMORY0);
 
-    return;  
+    return;
 }
 
 void HAL_DACEx_ConvM1CpltCallbackCh2(DAC_HandleTypeDef* hdac){
@@ -287,12 +287,12 @@ void HAL_DACEx_ConvM1CpltCallbackCh2(DAC_HandleTypeDef* hdac){
 
     HAL_DMAEx_ChangeMemory(&hdma_dac2, (uint32_t)SignalBuffer[index], MEMORY1);
 
-    return;  
+    return;
 }
 
 /**
  * @brief  System Clock Configuration
- *         The system Clock is configured as follow : 
+ *         The system Clock is configured as follow :
  *            System Clock source            = PLL (HSE)
  *            SYSCLK(Hz)                     = 180000000
  *            HCLK(Hz)                       = 180000000
@@ -311,51 +311,51 @@ void HAL_DACEx_ConvM1CpltCallbackCh2(DAC_HandleTypeDef* hdac){
  * @retval None
  */
 static void SystemClock_Config(void){
-	RCC_ClkInitTypeDef RCC_ClkInitStruct;
-	RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
 
-	/* Enable Power Control clock */
-	__PWR_CLK_ENABLE();
+    /* Enable Power Control clock */
+    __PWR_CLK_ENABLE();
 
-	/* The voltage scaling allows optimizing the power consumption when the device is 
-	   clocked below the maximum system frequency, to update the voltage scaling SignalPipe 
-	   regarding system frequency refer to product datasheet.  */
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /* The voltage scaling allows optimizing the power consumption when the device is
+       clocked below the maximum system frequency, to update the voltage scaling SignalPipe
+       regarding system frequency refer to product datasheet.  */
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-	/* Enable HSE Oscillator and activate PLL with HSE as source */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 8;
-	RCC_OscInitStruct.PLL.PLLN = 360;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 7;
-	HAL_RCC_OscConfig(&RCC_OscInitStruct);
+    /* Enable HSE Oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 360;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-	/* Activate the Over-Drive mode */
-	HAL_PWREx_ActivateOverDrive();
+    /* Activate the Over-Drive mode */
+    HAL_PWREx_ActivateOverDrive();
 
-	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
-	   clocks dividers */
-	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
-	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+       clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
 
-	__HAL_FLASH_PREFETCH_BUFFER_ENABLE();
+    __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
 
-	__HAL_FLASH_INSTRUCTION_CACHE_ENABLE(); 
-	__HAL_FLASH_DATA_CACHE_ENABLE();
+    __HAL_FLASH_INSTRUCTION_CACHE_ENABLE();
+    __HAL_FLASH_DATA_CACHE_ENABLE();
 }
 
 
 /*
 static void Error_Handler(void){
-	BSP_LED_On(LED4);
-	while(1);
+    BSP_LED_On(LED4);
+    while(1);
 }
 */
 
@@ -368,11 +368,11 @@ static void Error_Handler(void){
  * @retval None
  */
 void assert_failed(uint8_t* file, uint32_t line){
-	/* User can add his own implementation to report the file name and line number,
+    /* User can add his own implementation to report the file name and line number,
 ex: printf("Wrong parameters SignalPipe: file %s on line %d\r\n", file, line) */
 
-	/* Infinite loop */
-	while (1);
+    /* Infinite loop */
+    while (1);
 }
 #endif
 
