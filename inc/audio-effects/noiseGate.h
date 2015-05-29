dcbb9de728166ef141e1dce0da9fa90fad8373b4
sonicyang <sonic.tw.tp@gmail.com>
2015-05-29 17:20:15 +0800

    Add blank Image for noiseGate UI icon

--- inc/audio-effects/noiseGate.h
+++ inc/audio-effects/noiseGate.h
@@ -2,6 +2,7 @@
 #define __NOISEGATE_H__
 
 #include "base-effect.h"
+#include "noiseGate_img.bmp.h"
 
 struct NoiseGate_t{
     struct Effect_t parent;
@@ -12,6 +13,7 @@ struct Effect_t* new_NoiseGate();
 
 static struct EffectType_t NoiseGateId __attribute__((unused)) = {
     .name = "NoiseGate",
+    .image = noiseGate_img,
     .Init = new_NoiseGate
 };
 
